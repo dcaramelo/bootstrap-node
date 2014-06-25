c74ac0f3d02e1a34804cefe56dcf38928e4998fb
David Caramelo <david.caramelo83@gmail.com>
2014-06-25 02:07:44 +0200

    Inital commit

---
+++ Gruntfile.js
@@ -0,0 +1,116 @@
+'use strict';
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+    require('time-grunt')(grunt);
+
+    var appConfig = {
+        app: 'app',
+        dist: 'dist',
+        test: 'test'
+    };
+
+    grunt.initConfig({
+
+        // Project settings
+        bootstrap: appConfig,
+
+        // The actual grunt server settings
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    base: 'dist',
+                    hostname: '0.0.0.0',
+                    keepalive: true,
+                    middleware: function(connect, options) {
+                        return [
+                        connect.static(String(options.base)),
+                        connect.directory(String(options.base))];
+                    }
+                }
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            }
+        },
+        less: {
+            all: {
+                files: [ {
+                    expand: true,
+                    cwd: '<%= bootstrap.app %>/styles',
+                    src: 'main.less',
+                    dest: '<%= bootstrap.dist %>/styles',
+                    ext: ".css" }
+                ]
+            }
+        },
+        concat: {
+            js: {
+                src: '<%= bootstrap.app %>/js/**/*.js',
+                dest: '<%= bootstrap.dist %>/js/spec.js'
+            }
+        },
+        copy: {
+            index: {
+                expand: true,
+                cwd: '<%= bootstrap.app %>/',
+                src: '*.html',
+                dest: '<%= bootstrap.dist %>/',
+                filter: 'isFile'
+            }
+        },
+        bowercopy: {
+            options: {
+                srcPrefix: 'bower_components'
+            },
+            scripts: {
+                options: {
+                    destPrefix: '<%= bootstrap.dist %>/libs/js'
+                },
+                files: {
+                    'jquery.js': 'jquery/dist/jquery.js',
+                    'jquery-ui.js': 'jquery-ui/ui/jquery-ui.js',
+                    'angular.js': 'angular/angular.js',
+                    'angular-animate.js': 'angular-animate/angular-animate.js',
+                    'angular-cookies.js': 'angular-cookies/angular-cookies.js',
+                    'angular-resource.js': 'angular-resource/angular-resource.js',
+                    'angular-sanitize.js': 'angular-sanitize/angular-sanitize.js',
+                    'angular-touch.js': 'angular-touch/angular-touch.js',
+                    'angular-ui-router.js': 'angular-ui-router/release/angular-ui-router.js',
+                    'lodash.js': 'lodash/dist/lodash.js',
+                    'lodash.compat.js': 'lodash/dist/lodash.compat.js',
+                    'lodash.underscore.js': 'lodash/dist/lodash.underscore.js',
+                    'bootstrap.js': 'bootstrap/dist/js/bootstrap.js'
+                }
+            },
+            fonts: {
+                files: [{
+                    src: 'bootstrap/dist/fonts', dest: '<%= bootstrap.dist %>/libs/fonts'
+                }]
+            },
+            css: {
+                files: [
+                    { src: 'bootstrap/dist/css/bootstrap.css', dest: '<%= bootstrap.dist %>/libs/css'},
+                    { src: 'bootstrap/dist/css/bootstrap.css.map', dest: '<%= bootstrap.dist %>/libs/css'},
+                    { src: 'jquery-ui/themes/base/jquery-ui.css', dest: '<%= bootstrap.dist %>/libs/css'}
+                ]
+            },
+            images: {
+                files: [
+                    { src: 'jquery-ui/themes/base/images', dest: '<%= bootstrap.dist %>/libs/css/images'}
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bowercopy');
+
+    grunt.registerTask('build', ['concat', 'less', 'bowercopy', 'copy']);
+    grunt.registerTask('server', ['build', 'connect']);
+
+
+};
---
+++ app/js/app.js
@@ -0,0 +1,5 @@
+var applications = angular.module('bootstrap', []);
+
+    applications.controller('mainCtrl', function ($scope) {
+           $scope.hello = "Hello World !";
+        });
